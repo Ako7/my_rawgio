ef7d5ae61aee836383aedd6893119875db3a04ee
Ako7 <Sadowski123@gmail.com>
2024-04-03 18:40:09 +0200

    add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,19 @@
+import { GiBullseye } from "react-icons/gi";
+import { FaRegThumbsUp } from "react-icons/fa6";
+import { FaRegMeh } from "react-icons/fa";
+import { Icon, IconProps, Image, ImageProps } from "@chakra-ui/react";
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+  const emojiMap: { [key: number]: IconProps } = {
+    3: { children: <FaRegMeh />, color: "red" },
+    4: { children: <FaRegThumbsUp />, color: "yellow" },
+    5: { children: <GiBullseye />, color: "green" },
+  };
+  return <Icon {...emojiMap[rating]} boxSize="35px" marginTop={1} />;
+};
+
+export default Emoji;
