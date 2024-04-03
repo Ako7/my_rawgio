b7978040824bac7854174a03e0843ddd8f5350a8
Ako7 <Sadowski123@gmail.com>
2024-04-03 14:09:57 +0200

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from '../assets/rawg-logo.png' //add a placeholder image
 const getCroppedImageURL = (url: string) => {
-    if (!url) return '';
+    if (!url) return noImage;
     const target = 'media/'
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
