033b92e6211a3d23c3ef889c5a5bd686df2aed02
Ako7 <Sadowski123@gmail.com>
2024-04-15 15:14:45 +0200

    built game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import ApiClient, { FetchResponse } from "../services/apiClient";
+import { Screenshot } from "../entities/Screenshot";
+
+const useScreenshots = (gameId: number) => {
+    const apiClient = new ApiClient<Screenshot>(`/games/${gameId}/screenshots`);
+    return useQuery<FetchResponse<Screenshot>, Error>({
+        queryKey: ["screenshots", gameId],
+        queryFn: () => apiClient.getAll()
+    })
+}
+export default useScreenshots;
